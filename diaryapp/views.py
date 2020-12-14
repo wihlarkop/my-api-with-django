98c387d43e9329c56add4a91abe5dac24a00b40e
wihlarkop <wihlarko26@gmail.com>
2020-12-14 18:41:48 +0700

    update for api

--- diaryapp/views.py
+++ diaryapp/views.py
@@ -1,23 +1,8 @@
-from django.shortcuts import render, redirect
-
 from .models import DiaryPost
-from .forms import DiaryPostForm
 
 
 def list_diary_post(request):
     diarys = DiaryPost.objects.select_related('created_by').all()
-    form = DiaryPostForm(request.POST)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('diaryapp:list_diary_post')
-        else:
-            form = DiaryPostForm()
-    context = {
-        'diarys': diarys,
-        'form': form,
-    }
-    return render(request, 'diaryapp/index.html', context=context)
 
 
 def detail_diary_post(request):
