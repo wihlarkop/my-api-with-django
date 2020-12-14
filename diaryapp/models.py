f049d01fe5f3e47747c3d8a90ce25557f3f79ba5
wihlarkop <wihlarko26@gmail.com>
2020-12-14 18:53:06 +0700

    update diaryapp

--- diaryapp/models.py
+++ diaryapp/models.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-from ckeditor.fields import RichTextField
-
 
 class DiaryPost(models.Model):
     title = models.CharField(max_length=25)
-    content = RichTextField()
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -4,12 +4,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('account.urls', namespace='account')),
     path('blog/', include('blogapp.urls', namespace='blog')),
     path('diary/', include('diaryapp.urls', namespace='diary')),
     path('todo/', include('todoapp.urls', namespace='todo')),
     path('yellowpages/', include('yellowpagesapp.urls', namespace='yellowpages')),
-    path('portofolio/', include('portofolioapp.urls', namespace='portofolio'))
 ]
 
 if settings.DEBUG:
